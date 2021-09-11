991eefa8c01b5fe1c400d621ced7c0a94df57a89
phuongthuan <20045189+phuongthuan@users.noreply.github.com>
2021-09-11 22:20:18 +0700

    update telescope config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,18 +6,27 @@
 --- https://github.com/nvim-telescope/telescope.nvim
 
 local actions = require('telescope.actions')
-local builtin = require('telescope.builtin')
 
 require('telescope').setup{
   defaults = {
     file_sorter = require('telescope.sorters').get_fzy_sorter,
-    prompt_prefix = " >",
+    prompt_prefix = ' 🔍 ',
     color_devicons = true,
 
     file_previewer = require('telescope.previewers').vim_buffer_cat.new,
     grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
     qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
 
+    layout_config = {
+      horizontal = {
+        mirror = false,
+      },
+      vertical = {
+        mirror = false,
+      },
+      prompt_position = 'top',
+    },
+    borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
     vimgrep_arguments = {
       'rg',
       '--color=never',
@@ -29,11 +38,20 @@ require('telescope').setup{
     },
     mappings = {
       i = {
-        ['<C-n>'] = false,
-        ['<Esc>'] = actions.close,
+        ['<C-x>'] = false,
+        ['<C-j>'] = actions.move_selection_next,
+        ['<C-k>'] = actions.move_selection_previous,
         ['<C-q>'] = actions.send_to_qflist,
+        ['<Esc>'] = actions.close,
       }
     }
+  },
+  -- overriding default settings
+  pickers = {
+    find_files = {
+      theme = 'dropdown',
+      hidden = true
+    },
   },
   extensions = {
     fzy_native = {
@@ -47,14 +65,14 @@ require('telescope').load_extension('fzy_native')
 
 -- Mappings
 vim.cmd [[
+  nnoremap <C-p> :lua require('telescope.builtin').find_files()<CR>
   nnoremap <leader>ps :lua require('telescope.builtin').grep_string({ search = vim.fn.input("Grep For > ")})<CR>
-  nnoremap <leader>pf :lua require('telescope.builtin').find_files()<CR>
   nnoremap <leader>pw :lua require('telescope.builtin').grep_string { search = vim.fn.expand("<cword>") }<CR>
 
   nnoremap <leader>gg :lua require('telescope.builtin').git_status()<CR>
   nnoremap <leader>ht :lua require('telescope.builtin').help_tags()<CR>
   nnoremap <leader>C :lua require('telescope.builtin').commands()<CR>
-  nnoremap <leader>of :lua require('telescope.builtin').oldfiles()<CR>
+  nnoremap <leader>h :lua require('telescope.builtin').oldfiles()<CR>
   nnoremap <leader>bb :lua require('telescope.builtin').buffers()<CR>
   nnoremap <leader>gb :lua require('thuan.telescope').git_branches()<CR>
   nnoremap <leader>vrc :lua require('thuan.telescope').search_dotfiles()<CR>
