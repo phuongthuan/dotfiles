773a2d9573a9b56f6fbfad81031c6cd782a70ada
phuongthuan <20045189+phuongthuan@users.noreply.github.com>
2021-09-13 17:40:15 +0700

    update telescope icon and cleaning

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -11,7 +11,7 @@ local cmd = vim.cmd
 require('telescope').setup{
   defaults = {
     file_sorter = require('telescope.sorters').get_fzy_sorter,
-    prompt_prefix = ' 🔍 ',
+    prompt_prefix = ' 🔍  ',
     color_devicons = true,
 
     file_previewer = require('telescope.previewers').vim_buffer_cat.new,
