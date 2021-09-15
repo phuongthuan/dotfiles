ae8e706d0f32d6cbfe74c3a7e55078dd171604dd
phuongthuan <20045189+phuongthuan@users.noreply.github.com>
2021-09-15 21:26:34 +0700

    update lsp config for efm lang

--- nvim/lua/lsp/efm.lua
+++ nvim/lua/lsp/efm.lua
@@ -1,16 +1,40 @@
 local lspconfig = require('lspconfig')
 
+local prettier = {formatCommand = 'prettier --stdin-filepath ${INPUT}', formatStdin = true}
+
+local eslint = {
+    lintCommand = 'eslint_d -f visualstudio --stdin --stdin-filename ${INPUT}',
+    lintIgnoreExitCode = true,
+    lintStdin = true,
+    lintFormats = {'%f(%l,%c): %tarning %m', '%f(%l,%c): %trror %m'}
+}
+
+local rubocop = {
+    lintCommand = 'bundle exec rubocop --format emacs --force-exclusion --stdin ${INPUT}',
+    lintIgnoreExitCode = true,
+    lintStdin = true,
+    lintFormats = {'%f:%l:%c: %t: %m'},
+    formatCommand = 'bundle exec rubocop --auto-correct --force-exclusion --stdin ${INPUT} 2>/dev/null | sed "1,/^====================$/d"',
+    formatStdin = true
+}
+
+local filetypes = {'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'ruby', 'json', 'lua'}
+
 local M = {}
 
 M.setup = function(on_attach, capabilities)
-    -- Configure formatting for Lua
     lspconfig.efm.setup {
-        on_attach = on_attach,
-        init_options = {documentFormatting = true},
-        filetypes = {'lua'},
+        init_options = {documentFormatting = true, codeAction = true},
+        filetypes = filetypes,
         settings = {
-            rootMarkers = {'.git/'},
+            rootMarkers = {'package.json', '.git/', '.eslintrc.js'},
             languages = {
+                javascript = {prettier, eslint},
+                javascriptreact = {prettier, eslint},
+                typescript = {prettier, eslint},
+                typescriptreact = {prettier, eslint},
+                ruby = {rubocop},
+                json = {prettier},
                 lua = {
                     {
                         formatCommand = 'lua-format -i --no-keep-simple-function-one-line --no-break-after-operator --column-limit=150 --break-after-table-lb',
@@ -19,7 +43,9 @@ M.setup = function(on_attach, capabilities)
                 }
             }
         },
-        capabilities = capabilities
+        on_attach = on_attach,
+        capabilities = capabilities,
+        flags = {debounce_text_changes = 150}
     }
 end
 
--- nvim/lua/lsp/tsserver.lua
+++ nvim/lua/lsp/tsserver.lua
@@ -3,14 +3,14 @@ local lspconfig = require('lspconfig')
 local M = {}
 
 M.setup = function(on_attach, capabilities)
-  lspconfig.tsserver.setup {
-    on_attach = function(client)
-      client.resolved_capabilities.document_formatting = false
-      client.resolved_capabilities.document_range_formatting = false
-      on_attach(client)
-    end,
-    capabilities = capabilities
-  }
+    lspconfig.tsserver.setup {
+        on_attach = function(client)
+            client.resolved_capabilities.document_formatting = false
+            client.resolved_capabilities.document_range_formatting = false
+            on_attach(client)
+        end,
+        capabilities = capabilities
+    }
 end
 
 return M
