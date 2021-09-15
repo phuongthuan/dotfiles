64fc122a39611200cc71973e3df1f686b956a050
phuongthuan <20045189+phuongthuan@users.noreply.github.com>
2021-09-15 21:21:01 +0700

    replace vim-commentary with kommentary.nvim

---
+++ nvim/lua/plugins/kommentary.lua
@@ -0,0 +1,8 @@
+-----------------------------------------------------------
+-- Comment for vim
+-----------------------------------------------------------
+
+-- Plugin: kommentary
+--- https://github.com/b3nj5m1n/kommentary
+
+require('kommentary.config').use_extended_mappings()
