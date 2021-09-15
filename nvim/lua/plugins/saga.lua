8c74b9bef031726d623f3ee3f9a656b1028efa4e
phuongthuan <20045189+phuongthuan@users.noreply.github.com>
2021-09-15 21:19:46 +0700

    update saga mappings key

--- nvim/lua/plugins/saga.lua
+++ nvim/lua/plugins/saga.lua
@@ -1,28 +1,24 @@
 -----------------------------------------------------------
 -- lspsaga.nvim
 -----------------------------------------------------------
+
 -- A light-weight lsp plugin based on neovim built-in lsp with highly a performant UI.
 --- https://github.com/glepnir/lspsaga.nvim
-local saga = require 'lspsaga'
+
+local saga = require('lspsaga')
 local map = require('utils').map
-local cmd = vim.cmd
 
 saga.init_lsp_saga {
     code_action_prompt = {enable = true, sign = true, sign_priority = 20, virtual_text = false},
-    code_action_keys = {quit = {'q', '<ESC>'}, exec = '<CR>'},
+    code_action_keys = {quit = {'q', '<esc>'}, exec = '<CR>'},
     border_style = 'round'
 }
 
 map('n', 'K', ':Lspsaga hover_doc<CR>')
-map('n', '<C-k>', ':Lspsaga signature_help<CR>')
 map('n', 'gr', ':Lspsaga lsp_finder<CR>')
 map('n', '<leader>ca', ':Lspsaga code_action<CR>')
 map('n', '<leader>e', ':Lspsaga show_line_diagnostics<CR>')
 map('n', '<leader>rn', ':Lspsaga rename<CR>')
 
-map('n', ']]', ':Lspsaga diagnostic_jump_next<CR>')
-map('n', '[[', ':Lspsaga diagnostic_jump_next<CR>')
-
--- open/close terminal
-map('n', '<leader>T', ':Lspsaga open_floaterm<CR>')
-cmd [[tnoremap <silent> <leader>T <C-\><C-n>:Lspsaga close_floaterm<CR>]]
+map('n', ']e', ':Lspsaga diagnostic_jump_next<CR>')
+map('n', '[e', ':Lspsaga diagnostic_jump_next<CR>')
