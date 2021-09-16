29456a9184c3ba35968c61f979def9d893ad8fed
phuongthuan <20045189+phuongthuan@users.noreply.github.com>
2021-09-16 18:19:15 +0700

    config autocompletion for snippets

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -2,55 +2,60 @@
 -- Autocomplete configuration file
 -----------------------------------------------------------
 
--- Plugin: nvim-cmp
---- https://github.com/hrsh7th/nvim-cmp
-
 local cmp = require('cmp')
 local lspkind = require('lspkind')
 
+-- Plugin: nvim-cmp
+--- https://github.com/hrsh7th/nvim-cmp
+
 cmp.setup {
-  completion = {
-    completeopt = "menu,menuone,noinsert",
-  },
-  snippet = {
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body)
-    end,
-  },
-  mapping = {
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Insert,
-      select = true,
-    }),
-    ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
-    ['<Tab>'] = function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
-      elseif vim.fn['vsnip#available']() == 1 then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>(vsnip-expand-or-jump)', true, true, true), '')
-      else
-        fallback()
-      end
-    end,
-  },
-  formatting = {
-    format = function(_, vim_item)
-      vim_item.kind = lspkind.presets.default[vim_item.kind]
-      return vim_item
-    end
-  },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'nvim_lua' },
-    { name = 'vsnip' },
-    { name = 'path' },
-    { name = 'buffer' },
-  },
+    completion = {completeopt = 'menu,menuone,noinsert'},
+    snippet = {
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+        end
+    },
+    mapping = {
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.close(),
+        ['<CR>'] = cmp.mapping.confirm({behavior = cmp.ConfirmBehavior.Insert, select = true}),
+        ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'}),
+        ['<Tab>'] = function(fallback)
+            if vim.fn.pumvisible() == 1 then
+                vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
+            elseif vim.fn['vsnip#available']() == 1 then
+                vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>(vsnip-expand-or-jump)', true, true, true), '')
+            else
+                fallback()
+            end
+        end
+    },
+    formatting = {
+        format = function(entry, vim_item)
+            -- vim_item.kind = lspkind.presets.default[vim_item.kind] .. " "
+            vim_item.kind = lspkind.presets.default[vim_item.kind] .. " " .. vim_item.kind
+            vim_item.menu = ({
+                buffer = " [Buffer]",
+                path = " [Path]",
+                nvim_lsp = " [LSP]",
+                vsnip = " [Snippet]",
+                nvim_lua = "[Lua]",
+                calc = " [Calc]"
+            })[entry.source.name]
+            return vim_item
+        end
+    },
+    sources = {
+      {name = 'nvim_lsp'},
+      {name = 'nvim_lua'},
+      {name = 'path'},
+      {name = 'vsnip'},
+      {name = 'path'},
+      {name = 'buffer'}
+    }
 }
 
--- nvim/lua/plugins/vsnip.lua
+++ nvim/lua/plugins/vsnip.lua
@@ -2,17 +2,24 @@
 -- Vim Snippets
 -----------------------------------------------------------
 
--- Plugin: vim-vsnip
---- https://github.com/junegunn/fzf.vim
-
 local g = vim.g
+local map = require('utils').map
+local cmd = vim.cmd
 
-g.vsnip_filetypes = {
-  javascriptreact = { "javascript" },
-  typescriptreact = { "typescript" },
-}
+-- Plugin: vim-vsnip
+--- https://github.com/hrsh7th/vim-vsnip
 
-vim.cmd [[
+--[[ g.vsnip_filetypes = {
+  javascriptreact = { 'javascript' },
+  typescriptreact = { 'typescript' },
+} ]]
+
+map('n', '<leader>V', ':VsnipOpen<CR>')
+
+cmd [[
+  let g:vsnip_filetypes = {}
+  let g:vsnip_filetypes.javascriptreact = ['javascript']
+  let g:vsnip_filetypes.typescriptreact = ['typescript']
   imap <expr> <C-l>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'
   smap <expr> <C-l>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'
 ]]
