4d83417e6e4e6d002b431fbf554cd47117f1c348
phuongthuan <20045189+phuongthuan@users.noreply.github.com>
2021-09-18 08:42:21 +0700

    update telescope mappings key

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,82 +2,75 @@
 -- Telescope
 -----------------------------------------------------------
 
+local actions = require('telescope.actions')
+local map = require('utils').map
+
 -- Plugin: telescope.nvim
 --- https://github.com/nvim-telescope/telescope.nvim
 
-local actions = require('telescope.actions')
-local cmd = vim.cmd
+require('telescope').setup {
+    defaults = {
+        file_sorter = require('telescope.sorters').get_fzy_sorter,
+        prompt_prefix = ' 🔍  ',
+        color_devicons = true,
 
-require('telescope').setup{
-  defaults = {
-    file_sorter = require('telescope.sorters').get_fzy_sorter,
-    prompt_prefix = ' 🔍  ',
-    color_devicons = true,
+        file_previewer = require('telescope.previewers').vim_buffer_cat.new,
+        grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
+        qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
 
-    file_previewer = require('telescope.previewers').vim_buffer_cat.new,
-    grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
-    qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
+        file_ignore_patterns = {'node_modules', '.git'},
 
-    -- ignore these folders when searching
-    file_ignore_patterns = { 'node_modules', '.git/', '/public' },
-
-    layout_config = {
-      horizontal = {
-        mirror = false,
+        layout_config = {horizontal = {mirror = false}, vertical = {mirror = false}},
+        vimgrep_arguments = {'rg', '--color=never', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case'},
+        mappings = {
+            i = {
+                ['<C-x>'] = false,
+                ['<C-j>'] = actions.move_selection_next,
+                ['<C-k>'] = actions.move_selection_previous,
+                ['<C-q>'] = actions.send_to_qflist + actions.open_qflist,
+                ['<Esc>'] = actions.close
+            }
+        }
+    },
+    -- overriding default settings
+    pickers = {
+      find_files = {theme = 'dropdown', previewer = false},
+      buffers = {
+        theme = 'ivy',
+        previewer = false,
       },
-      vertical = {
-        mirror = false,
+      live_grep = {
+        prompt_title = 'Search Strings',
+        results_title = false,
       },
     },
-    vimgrep_arguments = {
-      'rg',
-      '--color=never',
-      '--no-heading',
-      '--with-filename',
-      '--line-number',
-      '--column',
-      '--smart-case'
-    },
-    mappings = {
-      i = {
-        ['<C-x>'] = false,
-        ['<C-j>'] = actions.move_selection_next,
-        ['<C-k>'] = actions.move_selection_previous,
-        ['<C-q>'] = actions.send_to_qflist,
-        ['<Esc>'] = actions.close,
-      }
-    }
-  },
-  -- overriding default settings
-  pickers = {
-    find_files = {
-      theme = 'dropdown',
-      previewer = false,
-    },
-  },
-  extensions = {
-    fzy_native = {
-      override_generic_sorter = false,
-      override_file_sorter = true,
-    },
-  },
+    extensions = {fzy_native = {override_generic_sorter = false, override_file_sorter = true}}
 }
 
 require('telescope').load_extension('fzy_native')
 
 -- Mappings
-cmd [[
-  nnoremap <C-p> :lua require('telescope.builtin').find_files()<CR>
-  nnoremap <leader>ps :lua require('telescope.builtin').live_grep()<CR>
-  nnoremap <leader>ht :lua require('telescope.builtin').help_tags()<CR>
-  nnoremap <leader>C :lua require('telescope.builtin').commands()<CR>
-  nnoremap <leader>h :lua require('telescope.builtin').oldfiles()<CR>
-  nnoremap <leader>sb :lua require('telescope.builtin').buffers()<CR>
 
-  nnoremap <leader>sdf :lua require('thuan.telescope').search_dotfiles()<CR>
-  nnoremap <leader>sn :lua require('thuan.telescope').search_notes()<CR>
+-- Telescope builtin function
+map('n', '<leader>tb', ':Telescope builtin<CR>')
 
-  nnoremap <leader>gg :lua require('telescope.builtin').git_status()<CR>
-  nnoremap <leader>gfc :lua require('telescope.builtin').git_commits()<CR>
-  nnoremap <leader>gb :lua require('telescope.builtin').git_branches()<CR>
-]]
+map('n', '<C-p>', ':lua require("telescope.builtin").find_files()<CR>')
+map('n', '<leader>ps', ':lua require("telescope.builtin").live_grep()<CR>')
+map('n', '<leader>pw', ':lua require("telescope.builtin").grep_string()<CR>')
+
+map('n', '<leader>C', ':lua require("telescope.builtin").commands()<CR>')
+map('n', '<leader>?', ':lua require("telescope.builtin").oldfiles()<CR>')
+map('n', '<leader>sb', ':lua require("telescope.builtin").buffers()<CR>')
+map('n', '<leader>sfb', ':lua require("telescope.builtin").file_browser()<CR>')
+
+-- Git
+map('n', '<leader>gg', ':lua require("telescope.builtin").git_status()<CR>')
+map('n', '<leader>gc', ':lua require("telescope.builtin").git_commits()<CR>')
+map('n', '<leader>gb', ':lua require("telescope.builtin").git_branches()<CR>')
+
+-- Custom function should require from thuan.telescope instead
+map('n', '<leader>sdf', ':lua require("thuan.telescope").search_dotfiles()<CR>')
+map('n', '<leader>sn', ':lua require("thuan.telescope").search_notes()<CR>')
+map('n', '<leader>snf', ':lua require("thuan.telescope").search_note_files()<CR>')
+map('n', '<leader>sr', ':lua require("thuan.telescope").search_references()<CR>')
+
--- nvim/lua/thuan/telescope.lua
+++ nvim/lua/thuan/telescope.lua
@@ -4,16 +4,32 @@ local M = {}
 
 M.search_dotfiles = function()
     builtin.find_files({
-        prompt_title = '< Dotfiles >',
-        cwd = '~/.dotfiles',
-        hidden = true,
+      prompt_title = '< Dotfiles >',
+      cwd = '~/.dotfiles',
+      hidden = true,
     })
 end
 
 M.search_notes = function()
-    builtin.find_files({
+    builtin.live_grep({
       prompt_title = '< Notes >',
       cwd = '~/vimwiki',
+      hidden = true
+    })
+end
+
+M.search_note_files = function()
+    builtin.find_files({
+      prompt_title = '< Find Notes>',
+      cwd = '~/vimwiki',
+      hidden = true
+    })
+end
+
+M.search_references = function()
+    builtin.find_files({
+      prompt_title = '< References >',
+      cwd = '~/Programming/references',
       hidden = true
     })
 end
