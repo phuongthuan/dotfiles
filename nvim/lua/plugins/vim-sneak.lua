6d9530440b78fb5d9fdf13729f43d9383988053d
phuongthuan <20045189+phuongthuan@users.noreply.github.com>
2021-09-21 12:32:56 +0700

    update sneak keymaps

--- nvim/lua/plugins/vim-sneak.lua
+++ nvim/lua/plugins/vim-sneak.lua
@@ -5,9 +5,11 @@
 local g = vim.g
 local map = require('utils').map
 
--- g["sneak#prompt"] = '🔎 '
+-- Plugin: justinmk/vim-sneak
+--- https://github.com/justinmk/vim-sneak
+
 g["sneak#label"] = true
 g["sneak#use_ic_scs"] = true
 
-map('n', 's', '<Plug>Sneak_s', {noremap = false})
-map('n', 'S', '<Plug>Sneak_S', {noremap = false})
+map('n', 'f', '<Plug>Sneak_s', {noremap = false})
+map('n', 'F', '<Plug>Sneak_S', {noremap = false})
