45a70a85d3c537f7541b5006163f98f44ee8cbe8
phuongthuan <20045189+phuongthuan@users.noreply.github.com>
2021-09-28 16:55:48 +0700

    update nvim tree settings

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -2,16 +2,15 @@
 -- File manager configuration file
 -----------------------------------------------------------
 
--- Plugin: nvim-tree
---- https://github.com/kyazdani42/nvim-tree.lua
-
 local g = vim.g
 local map = require('utils').map
 
-g.nvim_tree_width = 27
+-- Plugin: nvim-tree
+--- https://github.com/kyazdani42/nvim-tree.lua
+
+g.nvim_tree_width = 40
 g.nvim_tree_ignore = {'.git', 'node_modules', '.cache'}
 g.nvim_tree_gitignore = 1
-g.nvim_tree_indent_markers = 1
 g.nvim_tree_hide_dotfiles = 1
 g.nvim_tree_git_hl = 1
 g.nvim_tree_width_allow_resize  = 1
@@ -33,7 +32,11 @@ g.nvim_tree_icons = {
     deleted = "",
     ignored = "◌"
   },
+  folders = {
+    symlink= "",
+  }
 }
 
+-- Keymaps
 map('n', '<C-b>', ':NvimTreeToggle<CR>') -- open/close
 map('n', '<C-f>', ':NvimTreeFindFile<CR>') -- search file
