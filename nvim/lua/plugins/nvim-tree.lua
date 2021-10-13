b9a43d81071d21925c81af672837cbc893db5528
phuongthuan <20045189+phuongthuan@users.noreply.github.com>
2021-10-13 23:31:19 +0700

    update nvim-tree config

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -8,35 +8,28 @@ local map = require('utils').map
 -- Plugin: nvim-tree
 --- https://github.com/kyazdani42/nvim-tree.lua
 
-g.nvim_tree_width = 40
-g.nvim_tree_ignore = {'.git', 'node_modules', '.cache'}
+g.nvim_tree_width = 35
+g.nvim_tree_ignore = {'.git', 'node_modules', '.cache', '.DS_Store'}
 g.nvim_tree_gitignore = 1
 g.nvim_tree_hide_dotfiles = 1
 g.nvim_tree_git_hl = 1
 g.nvim_tree_width_allow_resize  = 1
 g.nvim_tree_special_files = {'README.md', 'Makefile', 'MAKEFILE'}
+
 g.nvim_tree_show_icons = {
-  git = 1,
-  folders = 1,
+  git = 0,
+  folders = 0,
   files = 1
 }
 
 g.nvim_tree_icons = {
-	default = "‣ ",
-  git = {
-    unstaged = "✗",
-    staged = "✓",
-    unmerged = "",
-    renamed = "➜",
-    untracked = "★",
-    deleted = "",
-    ignored = "◌"
+  default = '',
+  symlink = '',
+  folder = {
+    symlink = '',
   },
-  folders = {
-    symlink= "",
-  }
 }
 
 -- Keymaps
-map('n', '<C-b>', ':NvimTreeToggle<CR>') -- open/close
-map('n', '<C-f>', ':NvimTreeFindFile<CR>') -- search file
+map('n', '<C-b>', ':NvimTreeToggle<CR>')
+map('n', '<C-f>', ':NvimTreeFindFile<CR>')
