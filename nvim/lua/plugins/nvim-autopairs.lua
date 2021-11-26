4c9a0e26d44f0388a76d7c34eeec312110ddfebe
phuongthuan <20045189+phuongthuan@users.noreply.github.com>
2021-11-26 13:11:07 +0700

    config nvim autopairs with new neovim version 0.5.1

--- nvim/lua/plugins/nvim-autopairs.lua
+++ nvim/lua/plugins/nvim-autopairs.lua
@@ -5,13 +5,5 @@
 -- nvim-autopairs
 --- https://github.com/windwp/nvim-autopairs
 
-require('nvim-autopairs').setup({
-  check_ts = true,
-})
-
-require('nvim-autopairs.completion.cmp').setup({
-  map_cr = true,
-  map_complete = true, -- it will auto insert `(` after select function or method item
-  auto_select = false,
-})
+require('nvim-autopairs').setup{}
 
