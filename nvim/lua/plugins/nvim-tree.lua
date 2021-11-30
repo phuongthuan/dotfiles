6ccbf497baf7105175d19ceb59bd29f53f881695
phuongthuan <20045189+phuongthuan@users.noreply.github.com>
2021-11-30 13:19:50 +0700

    update nvim-tree config

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -8,10 +8,6 @@ local map = require('utils').map
 -- Plugin: nvim-tree
 --- https://github.com/kyazdani42/nvim-tree.lua
 
-g.nvim_tree_width = 40
-g.nvim_tree_ignore = {'.git', 'node_modules', '.cache', '.DS_Store'}
-g.nvim_tree_gitignore = 1
-g.nvim_tree_hide_dotfiles = 1
 g.nvim_tree_git_hl = 1
 g.nvim_tree_width_allow_resize  = 1
 g.nvim_tree_special_files = {'README.md', 'Makefile', 'MAKEFILE'}
@@ -30,7 +26,53 @@ g.nvim_tree_icons = {
   },
 }
 
-require'nvim-tree'.setup {}
+require'nvim-tree'.setup {
+  disable_netrw       = true,
+  hijack_netrw        = true,
+  open_on_setup       = false,
+  ignore_ft_on_setup  = {},
+  update_to_buf_dir   = {
+    enable = true,
+    auto_open = true,
+  },
+  auto_close          = false,
+  open_on_tab         = false,
+  hijack_cursor       = false,
+  update_cwd          = false,
+  diagnostics         = {
+    enable = false,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    }
+  },
+  update_focused_file = {
+    enable      = false,
+    update_cwd  = false,
+    ignore_list = {}
+  },
+  system_open = {
+    cmd  = nil,
+    args = {}
+  },
+  view = {
+    width = 40,
+    height = 30,
+    side = 'left',
+    auto_resize = false,
+    mappings = {
+      custom_only = false,
+      list = {}
+    }
+  },
+  filters = {
+    dotfiles = false,
+    custom = {}
+  }
+}
+
 
 -- Keymaps
 map('n', '<C-b>', ':NvimTreeToggle<CR>')
