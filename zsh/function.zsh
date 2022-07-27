d830308bdfe001f53a8adb370441057ef392fa77
phuongthuan <20045189+phuongthuan@users.noreply.github.com>
2022-07-28 01:13:15 +0700

    Update zsh config

--- zsh/function.zsh
+++ zsh/function.zsh
@@ -1,12 +1,10 @@
-# Simple commit :D
-function gcm() {
-  git add .;
-  git commit -m $1;
-  branch_name=$(git symbolic-ref --short -q HEAD);
-  git push origin $branch_name;
-}
-
 # Kill background process
 function bgkill() {
   kill -9 $(jobs -l | head -1 | awk '{print $3}')
 }
+
+# Open Github PR
+function mypr() {
+  open https://github.com/thinkei/frontend-core/pulls/phuongthuan
+}
+
