09a9217cd878d963f6a5f804b00d00307e2a3322
phuongthuan <20045189+phuongthuan@users.noreply.github.com>
2022-11-27 00:56:59 +0700

    Update treesitter config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,49 +1,49 @@
 -- Plugin: nvim-treesitter
 --- https://github.com/nvim-treesitter/nvim-treesitter
 
-require('nvim-treesitter.configs').setup({
-    indent = { enable = true },
-    highlight = { enable = true },
-    ensure_installed = {
-      "javascript",
-      "typescript",
-      "tsx",
-      "lua",
-      "json",
-      "jsonc",
-      "yaml",
-      "css",
-      "scss",
-      "ruby",
-      "html",
-    },
+local status, ts = pcall(require, 'nvim-treesitter.configs')
+if (not status) then return end
 
-    -- plugins
-    autopairs = { enable = true },
-    textsubjects = {
-      enable = true,
-      keymaps = {
-        ["."] = "textsubjects-smart",
-        [";"] = "textsubjects-container-outer",
-      },
-    },
-    autotag = {
-       enable = true,
-    },
-    -- enable module 'nvim-ts-context-commentstring' in treesitter
-    context_commentstring = {
-      enable = true,
-      config = {
-        javascript = {
-          __default = '// %s',
-          jsx_element = '{/* %s */}',
-          jsx_fragment = '{/* %s */}',
-          jsx_attribute = '// %s',
-          comment = '// %s'
-        }
+ts.setup {
+  highlight = {
+    enable = true,
+    disable = {},
+  },
+  indent = {
+    enable = true,
+    disable = {},
+  },
+  ensure_installed = {
+      'javascript',
+      'typescript',
+      'tsx',
+      'lua',
+      'json',
+      'yaml',
+      'css',
+      'scss',
+      'ruby',
+      'html',
+  },
+  autotag = {
+    enable = true,
+  },
+  autopairs = { enable = true },
+
+  -- enable module 'nvim-ts-context-commentstring' in treesitter
+  context_commentstring = {
+    enable = true,
+    config = {
+      javascript = {
+        __default = '// %s',
+        jsx_element = '{/* %s */}',
+        jsx_fragment = '{/* %s */}',
+        jsx_attribute = '// %s',
+        comment = '// %s'
       }
     }
-})
+  }
+}
 
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
+local parser_config = require 'nvim-treesitter.parsers'.get_parser_configs()
+parser_config.tsx.filetype_to_parsername = { 'javascript', 'typescript.tsx' }
