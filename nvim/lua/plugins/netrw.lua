9fd7e5f492db236d2ff886b000128ca243e24320
phuongthuan <20045189+phuongthuan@users.noreply.github.com>
2022-12-02 02:01:55 +0700

    Add netrw

---
+++ nvim/lua/plugins/netrw.lua
@@ -0,0 +1,14 @@
+local g = vim.g
+
+require'netrw'.setup {
+    -- Put your configuration here, or leave the object empty to take the default
+    -- configuration.
+    icons = {
+        symlink = '',
+        directory = '',
+        file = ''
+    },
+    use_devicons = true,
+    mappings = {}
+}
+
