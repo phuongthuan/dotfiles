dda569acc2fec16f4e4d8274c21451cbd1b04ca1
phuongthuan <20045189+phuongthuan@users.noreply.github.com>
2023-09-25 23:00:48 +0700

    add alias function for git

--- zsh/function.zsh
+++ zsh/function.zsh
@@ -3,8 +3,36 @@ function bgkill() {
   kill -9 $(jobs -l | head -1 | awk '{print $3}')
 }
 
-# Open Github PR
+# Open Github PR: mypr <project_name>
 function mypr() {
-  open https://github.com/thinkei/frontend-core/pulls/phuongthuan
+  if [ -z "$1" ]; then
+    open 'https://github.com/thinkei/frontend-core/pulls/phuongthuan'
+  else
+    project="$1"
+    open "https://github.com/thinkei/${project}/pulls/phuongthuan"
+  fi
 }
 
+# Open Circle CI: myci <project_name>
+function myci() {
+  if [ -z "$1" ]; then
+    open 'https://app.circleci.com/pipelines/github/Thinkei?filter=mine'
+  else
+    project="$1"
+    open "https://app.circleci.com/pipelines/github/Thinkei/${project}?filter=mine"
+  fi
+}
+
+# Simple Git commit: mygcm "commit message"
+function mygcm() {
+  # Check if a commit message is provided as an argument
+  if [ -z "$1" ]; then
+    echo "Please provide a commit message."
+    return 1
+  fi
+
+  message="$1"
+  git add .
+  git commit -m "$message"
+  git push origin HEAD
+}
