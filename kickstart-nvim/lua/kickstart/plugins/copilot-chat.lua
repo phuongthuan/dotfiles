09aa9c47b76df322c063b7bbde2a4ebfedb5d041
phuongthuan <20045189+phuongthuan@users.noreply.github.com>
2024-06-17 21:50:36 +0700

    adding copilot chat

---
+++ kickstart-nvim/lua/kickstart/plugins/copilot-chat.lua
@@ -0,0 +1,15 @@
+return {
+  'CopilotC-Nvim/CopilotChat.nvim',
+  branch = 'canary',
+  dependencies = {
+    { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
+    { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+  },
+  config = function()
+    require('CopilotChat').setup {
+      debug = true,
+    }
+
+    vim.keymap.set('n', '<leader>cc', '<cmd>CopilotChat<cr>')
+  end,
+}
--- kickstart-nvim/lua/lazy-plugins.lua
+++ kickstart-nvim/lua/lazy-plugins.lua
@@ -64,6 +64,8 @@ require('lazy').setup({
 
   require 'kickstart.plugins.copilot',
 
+  require 'kickstart.plugins.copilot-chat',
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
