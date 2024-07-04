c6173dce0784ec7e120e44086324cb511dd9d853
phuongthuan <20045189+phuongthuan@users.noreply.github.com>
2024-07-04 23:40:44 +0700

    update noice config

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -12,9 +12,24 @@ return {
     },
     config = function()
       require('noice').setup {
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+            ['vim.lsp.util.stylize_markdown'] = true,
+            ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+          },
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+          bottom_search = true, -- use a classic bottom cmdline for search
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false, -- add a border to hover docs and signature help
+        },
         cmdline = {
           format = {
-            cmdline = { pattern = '^:', icon = '🖥️', lang = 'vim' },
+            cmdline = { pattern = '^:', icon = '💻', lang = 'vim' },
             search_down = { kind = 'search', pattern = '^/', icon = '🔍 ', lang = 'regex' },
             search_up = { kind = 'search', pattern = '^%?', icon = '🔍 ', lang = 'regex' },
             input = { view = 'cmdline_input', icon = '📝' }, -- Used by input()k
