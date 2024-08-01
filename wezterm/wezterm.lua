c7421d893223cb63fb8bb7060f3bd3d130776c1f
phuongthuan <20045189+phuongthuan@users.noreply.github.com>
2024-08-01 20:06:37 +0700

    update editor config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,29 +2,11 @@ local wezterm = require("wezterm")
 
 return {
 	-- Config font
-	font = wezterm.font("Mononoki Nerd Font Mono", { weight = "Bold" }),
-	-- font = wezterm.font_with_fallback({
-	-- 	"Mononoki Nerd Font Mono",
-	-- 	"Fira Code",
-	-- }),
-	font_size = 12.0,
-	-- font_rules = {
-	-- 	{
-	-- 		intensity = "Bold",
-	-- 		font = wezterm.font("Mononoki Nerd Font Mono", { weight = "Bold" }),
-	-- 	},
-	-- 	{
-	-- 		italic = true,
-	-- 		font = wezterm.font("Mononoki Nerd Font Mono", { italic = true }),
-	-- 	},
-	-- 	{
-	-- 		italic = true,
-	-- 		intensity = "Bold",
-	-- 		font = wezterm.font("Mononoki Nerd Font Mono", { weight = "Bold", italic = true }),
-	-- 	},
-	-- },
+	font = wezterm.font("Dank Mono", { weight = "Medium" }),
+	font_size = 13.5,
 
 	color_scheme = "GruvboxDark",
+	-- color_scheme = "Gruvbox light, soft (base16)",
 	hide_tab_bar_if_only_one_tab = true,
 	window_decorations = "RESIZE",
 	enable_scroll_bar = false,
