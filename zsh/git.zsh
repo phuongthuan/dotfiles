d0f0c1b4583b9049a458dfd49e81777dee7cab11
phuongthuan <20045189+phuongthuan@users.noreply.github.com>
2025-03-19 13:30:28 +0700

    update zsh config

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -7,7 +7,7 @@ fi
 # Open repo by project name: github_project_url <project_name>
 github_project_url() {
   if [ -z "$1" ]; then
-    echo "Please provide a project name 🔴"
+    echo -e "\033[31mPlease provide a project name \033[0m"
     return 1
   fi
   open "$EH_GITHUB_URL/$1"
@@ -52,16 +52,6 @@ orp() {
   fi
 }
 
-# Open workflows: oga <env> <project_name> <branch_name>
-# oga() {
-#   if [ -z "$1" ]; then
-#     open "https://github.com/Thinkei/${EH_DEFAULT_PROJECT}/actions/workflows/build-sandbox--hr-web-app.yml?query=actor%3Aphuongthuan"
-#   else
-#     project="$1"
-#     open "https://github.com/Thinkei/${project}/actions/workflows/build-sandbox--hr-web-app.yml?query=actor%3Aphuongthuan"
-#   fi
-# }
-
 # Open Run NPM release workflow: orl <env> <project_name> <branch_name>
 orl() {
   if [ -z "$1" ]; then
@@ -91,20 +81,23 @@ push_and_open_pr() {
   response=$(git push origin HEAD --no-verify 2>&1)
 
   # Extract the URL from the output
-  url=$(echo "$response" | grep -oE 'https://github\.com/[^[:space:]]+/pull/new/[^[:space:]]+')
+  new_pr_url=$(echo "$response" | grep -oE 'https://github\.com/[^[:space:]]+/pull/new/[^[:space:]]+')
 
-  echo "⛳ Opening PR in Browser..."
-  echo -e "\033[32m$url \033[0m"
-
-  # Open the URL in the default browser if it exists
-  [ -n "$url" ] && open "$url"
+  if [ -n "$new_pr_url" ]; then
+    echo "⛳ Opening PR in Browser..."
+    echo -e "\033[32m$new_pr_url \033[0m"
+    open "$new_pr_url"
+  else
+    # Open pull request of current branch
+    oprb
+  fi
 }
 
 # Create a commit: cgc "commit message"
 cgc() {
   # Check if a commit message is provided as an argument
   if [ -z "$1" ]; then
-    echo "Please provide a commit message 🔴"
+    echo -e "\033[31mPlease provide a commit message \033[0m"
     return 1
   fi
 
@@ -139,20 +132,20 @@ ccn() {
 ccb() {
   current_branch=$(git branch --show-current)
   echo "$current_branch" | pbcopy
-  echo "Copied current branch to clipboard 📝"
+  echo -e "\033[32mCopied current branch to clipboard 📝 \033[0m"
 }
 
 # Create a branch and copy branch name to clipboard
 cgb() {
   if [ -z "$1" ]; then
-    echo "Please provide a branch name 🔴"
+    echo -e "\033[31mPlease provide a branch name \033[0m"
     return 1
   fi
 
   branch_name="$1"
   git branch $branch_name
   echo "$branch_name" | pbcopy
-  echo "Copied branch '$branch_name' to clipboard 📝"
+  echo -e "\033[32mCopied branch $branch_name to clipboard 📝 \033[0m"
 }
 
 # Reset master branch
@@ -166,7 +159,7 @@ clear_all_local_branches_except() {
   excluded_branches="$1"
 
   if [ -z "$excluded_branches" ]; then
-    echo "Please provide a branch 🔴"
+    echo -e "\033[31mPlease provide a branch \033[0m"
     return 1
   fi
 
@@ -189,7 +182,7 @@ oprb() {
 
   # Check if gh CLI is installed
   if ! command -v gh &>/dev/null; then
-    echo "GitHub CLI (gh) is not installed. Please install it to use this function."
+    echo -e "\033[31mGitHub CLI (gh) is not installed. Please install it to use this function. \033[0m"
     return 1
   fi
 
@@ -198,10 +191,9 @@ oprb() {
   pr_url=$(gh pr view "$branch_name" --json url --jq '.url')
 
   if [ -n "$pr_url" ]; then
-    echo "Opening PR: $pr_url"
-    open "$pr_url"
+    echo "⛳ Opening PR in Browser..."
+    echo -e "\033[32m$pr_url \033[0m"
   else
-    echo "No PR found for branch '$branch_name'"
     return 1
   fi
 }
@@ -214,67 +206,7 @@ oga() {
     echo "📦 Opening GitHub Actions talent-marketplace-app workflow: $workflow"
     open "https://github.com/Thinkei/talent-marketplace-app/actions/workflows/$workflow"
   else
-    echo "Please enter workflow name"
+    echo -e "\033[31mPlease enter workflow name \033[0m"
     return 1
-  fi
-}
-
-# Running linter in given directory (default dir is apps/hr-web-app)
-# fe_test <directory>
-#
-# fe_test apps/hr-web-app
-# fe_test libs/eh-marketing-ui
-fe_lint() {
-
-  # Use the provided argument or default to 'apps/hr-web-app'
-  directory=${1:-apps/hr-web-app}
-
-  # Extract 'hr-web-app' from 'apps/hr-web-app'
-  app_name=$(basename "$directory")
-
-  echo "Start linting on $app_name 🧪"
-
-  executed_files=$(
-    # Lists all files that have been added, modified, copied, renamed, or had type changes compared to the master branch.
-    git diff --name-only --diff-filter=ACMRT master |
-      grep "^$directory/" |          # Filter all files that start with a directory (eg: apps/hr-web-app)
-      grep -E '\.(js|jsx|ts|tsx)$' | # Only included js,jsx,ts,tsx file
-      sed "s|$directory/||"          # Remove path directory (eg: apps/hr-web-app)
-  )
-
-  if [[ -n "$executed_files" ]]; then
-    yarn nx lint:ci $app_name $(echo "$executed_files" | xargs)
-  else
-    echo "No matching files to lint 🔴"
-  fi
-}
-
-# Running unit test on given directory (default dir is apps/hr-web-app)
-# fe_test <directory>
-#
-# fe_test apps/hr-web-app
-# fe_test libs/eh-marketing-ui
-fe_test() {
-
-  # Use the provided argument or default to 'apps/hr-web-app'
-  directory=${1:-apps/hr-web-app}
-
-  # Extract 'hr-web-app' from 'apps/hr-web-app'
-  app_name=$(basename "$directory")
-
-  echo "Start testing on $app_name 🧪"
-
-  executed_files=$(
-    # Lists all files that have been added, modified, copied, renamed, or had type changes compared to the master branch.
-    git diff --name-only --diff-filter=ACMRT master |
-      grep "^$directory/" |                       # Filter all files that start with a directory (eg: apps/hr-web-app)
-      grep -E '\.(spec|test)\.(js|jsx|ts|tsx)$' | # Only included test files
-      sed "s|$directory/||"                       # Remove path directory (eg: apps/hr-web-app)
-  )
-
-  if [[ -n "$executed_files" ]]; then
-    yarn nx test:ci $app_name $(echo "$executed_files" | xargs) --skip-nx-cache
-  else
-    echo "No matching files to test 🔴"
   fi
 }
