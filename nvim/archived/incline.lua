1b1d49c7aee4665903b10765a0c99e5d54adae93
phuongthuan <20045189+phuongthuan@users.noreply.github.com>
2025-06-04 22:51:55 +0700

    archived package

---
+++ nvim/archived/incline.lua
@@ -0,0 +1,46 @@
+return {
+  -- Adding a filename to the top right corner
+  {
+    'b0o/incline.nvim',
+    enabled = true,
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      local helpers = require('incline.helpers')
+      local devicons = require('nvim-web-devicons')
+
+      require('incline').setup({
+        hide = {
+          only_win = false,
+        },
+        window = {
+          margin = {
+            vertical = 2,
+            horizontal = 1,
+          },
+          placement = {
+            horizontal = 'right',
+            vertical = 'top',
+          },
+        },
+        render = function(props)
+          local bufname = vim.api.nvim_buf_get_name(props.buf)
+          local filename = vim.fn.fnamemodify(bufname, ':t')
+
+          if filename == '' then
+            filename = '[No Name]'
+          end
+
+          local ft_icon, ft_color = devicons.get_icon_color(filename)
+          local modified = vim.bo[props.buf].modified
+          return {
+            ft_icon and { ' ', ft_icon, ' ', guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or '',
+            ' ',
+            { filename, gui = modified and 'bold,italic' or 'regular' },
+            ' ',
+            guibg = '#3c3836',
+          }
+        end,
+      })
+    end,
+  },
+}
